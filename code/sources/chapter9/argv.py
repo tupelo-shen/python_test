314b0ff615482697a924338f54df339505d1c590
Wanjiang.Shen@cn.yokogawa.com <Wanjiang.Shen@cn.yokogawa.com>
2018-01-08 13:35:41 +0800

    2018/01/08

--- code/sources/chapter9/argv.py
+++ code/sources/chapter9/argv.py
@@ -1,34 +1,6 @@
 #!/usr/bin/env python  # start line
-# module doc
-# imported modules
-import sys,getopt
-# variable definitions
-# class definition
-# function definition
-# main program
-def main():
-    proc_cmd_args()
 
-def usage():
-    print 'Usage: python argv.py [-s <xyz>] arg1[,arg2..]'
-    print 'Options:'
-    print '    -h, --help       show this help message and exit'
-    print '    -i, --ip         server ip'
-    print '    -p, --port       server port'
-    
-def proc_cmd_args():
-    try:
-        options,args = getopt.getopt(sys.argv[1:],"hp:i:",["help","ip=","port="])
-    except getopt.GetoptError:
-        sys.exit()
+import sys
 
-    for name,value in options:
-        if name in ("-h","--help"):
-            usage()
-        if name in ("-i","--ip"):
-            print 'ip is----',value
-        if name in ("-p","--port"):
-            print 'port is----',value
-
-if __name__ == '__main__':
-    main()
+print 'you entered', len(sys.argv), 'arguments...'
+print 'they were:', str(sys.argv)
---
+++ code/sources/chapter9/argv_getopt.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python  # start line
+# module doc
+# imported modules
+import sys,getopt
+# variable definitions
+# class definition
+# function definition
+# main program
+def main():
+    proc_cmd_args()
+
+def usage():
+    print 'Usage: python argv.py [-s <xyz>] arg1[,arg2..]'
+    print 'Options:'
+    print '    -h, --help       show this help message and exit'
+    print '    -i, --ip         server ip'
+    print '    -p, --port       server port'
+    
+def proc_cmd_args():
+    try:
+        options,args = getopt.getopt(sys.argv[1:],"hp:i:",["help","ip=","port="])
+    except getopt.GetoptError:
+        sys.exit()
+
+    for name,value in options:
+        if name in ("-h","--help"):
+            usage()
+        if name in ("-i","--ip"):
+            print 'ip is----',value
+        if name in ("-p","--port"):
+            print 'port is----',value
+
+if __name__ == '__main__':
+    main()
